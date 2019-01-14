261272e4ce13f65e577352c216ea0037e815f10b
Kuldeep M <crudetoaster@gmail.com>
2019-01-14 17:32:50 +0000

    [feature] adding random accent color options

--- js/theme.js
+++ js/theme.js
@@ -8,11 +8,31 @@ var theme = (function() {
 
   var random = function() {
     if (state.get().layout.theme.random) {
-      var randomColor = {
-        r: helper.randomNumber(0, 255),
-        g: helper.randomNumber(0, 255),
-        b: helper.randomNumber(0, 255)
+      var randomVal = function(min, max) {
+        return Math.floor(Math.random() * (max - min) + 1) + min;
       };
+      var color = {
+        light: function() {
+          return {
+            h: randomVal(0, 360),
+            s: randomVal(50, 100),
+            l: randomVal(50, 90)
+          };
+        },
+        dark: function() {
+          return {
+            h: randomVal(0, 360),
+            s: randomVal(0, 50),
+            l: randomVal(0, 40)
+          };
+        }
+      };
+      var hsl = color.light();
+      var randomColor = helper.hslToRgb({
+        h: hsl.h,
+        s: (hsl.s / 100),
+        l: (hsl.l / 100)
+      });
       state.change({
         path: "layout.theme.current",
         value: randomColor
