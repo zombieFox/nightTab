96920828d1cc881a8ee4e0f2a9ae82795275a944
zombieFox <crudetoaster@gmail.com>
2019-01-22 00:48:15 +0000

    [feature] add options to hide bookmarks

--- js/background.js
+++ js/background.js
@@ -1,17 +1,27 @@
 var background = (function() {
 
   var render = function() {
+    console.log(state.get().background.image.blur);
     var html = helper.e("html");
     if (state.get().background.image.active && state.get().background.image.url != "") {
-        html.style.setProperty("--background-image", "url(\"" + state.get().background.image.url + "\")");
-        html.style.setProperty("--background-opacity", state.get().background.image.opacity);
-        html.style.setProperty("--background-blur", state.get().background.image.blur + "px");
-        html.style.setProperty("--background-accent-opacity", state.get().background.image.accentOpacity);
+      html.style.setProperty("--background-image", "url(\"" + state.get().background.image.url + "\")");
     } else {
       html.style.setProperty("--background-image", "none");
-      html.style.setProperty("--background-opacity", 1);
-      html.style.setProperty("--background-blur", 0);
-      html.style.setProperty("--background-accent-opacity", 0);
+    };
+    if (state.get().background.image.blur > 0) {
+      html.style.setProperty("--background-blur", state.get().background.image.blur + "px");
+    } else {
+      html.style.setProperty("--background-blur", "none");
+    };
+    if (state.get().background.image.opacity < 1) {
+      html.style.setProperty("--background-opacity", state.get().background.image.opacity);
+    } else {
+      html.style.setProperty("--background-opacity", "none");
+    };
+    if (state.get().background.image.accentOpacity > 0) {
+      html.style.setProperty("--background-accent-opacity", state.get().background.image.accentOpacity);
+    } else {
+      html.style.setProperty("--background-accent-opacity", "none");
     };
   };
 
