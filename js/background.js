069898ea0d7f7f521a1e15029efe168fc579116f
zombieFox <crudetoaster@gmail.com>
2019-01-22 01:00:05 +0000

    [feature] add options to hide bookmarks

--- js/background.js
+++ js/background.js
@@ -1,26 +1,35 @@
 var background = (function() {
 
   var render = function() {
-    console.log(state.get().background.image.blur);
     var html = helper.e("html");
-    if (state.get().background.image.active && state.get().background.image.url != "") {
-      html.style.setProperty("--background-image", "url(\"" + state.get().background.image.url + "\")");
+    var background = helper.e(".background");
+    if (state.get().background.image.active) {
+      helper.removeClass(background, "is-hidden");
+      if (state.get().background.image.url != "") {
+        html.style.setProperty("--background-image", "url(\"" + state.get().background.image.url + "\")");
+      } else {
+        html.style.setProperty("--background-image", "none");
+      };
+      if (state.get().background.image.blur > 0) {
+        html.style.setProperty("--background-blur", state.get().background.image.blur + "px");
+      } else {
+        html.style.setProperty("--background-blur", "none");
+      };
+      if (state.get().background.image.opacity < 1) {
+        html.style.setProperty("--background-opacity", state.get().background.image.opacity);
+      } else {
+        html.style.setProperty("--background-opacity", "none");
+      };
+      if (state.get().background.image.accentOpacity > 0) {
+        html.style.setProperty("--background-accent-opacity", state.get().background.image.accentOpacity);
+      } else {
+        html.style.setProperty("--background-accent-opacity", "none");
+      };
     } else {
+      helper.addClass(background, "is-hidden");
       html.style.setProperty("--background-image", "none");
-    };
-    if (state.get().background.image.blur > 0) {
-      html.style.setProperty("--background-blur", state.get().background.image.blur + "px");
-    } else {
       html.style.setProperty("--background-blur", "none");
-    };
-    if (state.get().background.image.opacity < 1) {
-      html.style.setProperty("--background-opacity", state.get().background.image.opacity);
-    } else {
       html.style.setProperty("--background-opacity", "none");
-    };
-    if (state.get().background.image.accentOpacity > 0) {
-      html.style.setProperty("--background-accent-opacity", state.get().background.image.accentOpacity);
-    } else {
       html.style.setProperty("--background-accent-opacity", "none");
     };
   };
