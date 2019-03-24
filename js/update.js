32a3b0fe83ddad3123d505d102b46a52e9ed981f
zombieFox <crudetoaster@gmail.com>
2019-03-24 22:58:28 +0000

    [feature] improve version checking

--- js/update.js
+++ js/update.js
@@ -155,48 +155,59 @@ var update = (function() {
       console.log("\t -- running update", 1.00);
       data = _update_100(data);
     };
-    if (data.version < 2.00) {
-      console.log("\t -- running update", 2.00);
-      data = _update_200(data);
-    };
-    if (data.version < 2.10) {
-      console.log("\t -- running update", 2.10);
-      data = _update_210(data);
-    };
-    if (data.version < 2.30) {
-      console.log("\t -- running update", 2.30);
-      data = _update_230(data);
-    };
-    if (data.version < 2.40) {
-      console.log("\t -- running update", 2.40);
-      data = _update_240(data);
-    };
-    if (data.version < 2.50) {
-      console.log("\t -- running update", 2.50);
-      data = _update_250(data);
-    };
-    if (data.version < 2.70) {
-      console.log("\t -- running update", 2.70);
-      data = _update_270(data);
-    };
-    if (data.version < 2.80) {
-      console.log("\t -- running update", 2.80);
-      data = _update_280(data);
-    };
-    if (data.version < 2.90) {
-      console.log("\t -- running update", 2.90);
-      data = _update_290(data);
-    };
-    if (data.version < 2.91) {
-      console.log("\t -- running update", 2.91);
-      data.version = 2.91;
+    if (typeof data.version == "number") {
+      console.log(1);
+      if (data.version < 2.00) {
+        console.log("\t -- running update", 2.00);
+        data = _update_200(data);
+      };
+      if (data.version < 2.10) {
+        console.log("\t -- running update", 2.10);
+        data = _update_210(data);
+      };
+      if (data.version < 2.30) {
+        console.log("\t -- running update", 2.30);
+        data = _update_230(data);
+      };
+      if (data.version < 2.40) {
+        console.log("\t -- running update", 2.40);
+        data = _update_240(data);
+      };
+      if (data.version < 2.50) {
+        console.log("\t -- running update", 2.50);
+        data = _update_250(data);
+      };
+      if (data.version < 2.70) {
+        console.log("\t -- running update", 2.70);
+        data = _update_270(data);
+      };
+      if (data.version < 2.80) {
+        console.log("\t -- running update", 2.80);
+        data = _update_280(data);
+      };
+      if (data.version < 2.90) {
+        console.log("\t -- running update", 2.90);
+        data = _update_290(data);
+      };
+      if (data.version < 2.91) {
+        console.log("\t -- running update", 2.91);
+        data.version = 2.91;
+      };
+      if (data.version < 2.96) {
+      // if (version.compare(version.get(), data.version)) {
+        console.log("\t -- running update", "2.10.0");
+        data.version = "2.10.0";
+      };
+    } else if (typeof data.version == "string") {
+      console.log(2);
+      console.log(version.compare(data.version, version.get()));
     };
     // if no update is needed
     // version bump
-    if (data.version < version.get()) {
-      console.log("\t -- nothing to update, version bump to", version.get());
-      data.version = version.get();
-    };
+    // if (data.version < version.get()) {
+    //   console.log("\t -- nothing to update, version bump to", version.get());
+    //   data.version = version.get();
+    // };
     return data;
   };
 
