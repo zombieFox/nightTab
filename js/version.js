1ab055962ac09acb19c54ac65dad847a534cc452
Kuldeep M <crudetoaster@gmail.com>
2019-04-02 12:23:47 +0100

    [feature] adding title tag to url text

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 var version = (function() {
 
   // version is normally bumped when the state needs changing or any new functionality is added
-  var current = "2.12.2";
+  var current = "2.13.0";
 
   var compare = function(a, b) {
     var pa = a.split(".");
