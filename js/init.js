852c1ec90e3d724e66a2b4fd9854c2f33496f78d
Tim <teiem@yahoo.com>
2019-06-19 02:15:16 +0200

    removed leftover

--- js/init.js
+++ js/init.js
@@ -1,24 +1,20 @@
 // log version
 console.log("nightTab version", version.get());
-const init = () => {
-    data.init();
-    state.init();
-    bookmarks.init();
-    menu.init();
-    accent.init();
-    link.init();
-    control.init();
-    greeting.init();
-    transitional.init();
-    date.init();
-    clock.init();
-    keyboard.init();
-    layout.init();
-    background.init();
-    search.init();
-    title.init();
-    header.init();
-    theme.init();
-};
-
-init();
+data.init();
+state.init();
+bookmarks.init();
+menu.init();
+accent.init();
+link.init();
+control.init();
+greeting.init();
+transitional.init();
+date.init();
+clock.init();
+keyboard.init();
+layout.init();
+background.init();
+search.init();
+title.init();
+header.init();
+theme.init();
