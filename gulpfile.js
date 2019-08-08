ebbc1068c0b0a15adfa52ca5dc02c0d6cec57732
Kuldeep M <crudetoaster@gmail.com>
2019-08-08 12:23:40 +0100

    [refactor] reduce gulp folder object complexity

--- gulpfile.js
+++ gulpfile.js
@@ -8,91 +8,77 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
 const folder = {
-  src: {
-    root: 'src'
-  },
-  build: {
-    root: 'build/web'
-  }
+  src: 'src',
+  build: 'build/web'
 }
-folder.src.icons = folder.src.root + '/icons';
-folder.src.fonts = folder.src.root + '/fonts';
-folder.src.css = folder.src.root + '/css';
-folder.src.js = folder.src.root + '/js';
-folder.build.icons = folder.build.root + '/icons';
-folder.build.fonts = folder.build.root + '/fonts';
-folder.build.css = folder.build.root + '/css';
-folder.build.js = folder.build.root + '/js';
 const filename = {
   css: "nighttab.min.css",
   js: "nighttab.min.js"
 }
 
 function manifest() {
-  return src(folder.src.root + '/manifest.json')
-    .pipe(dest(folder.build.root))
+  return src(folder.src + '/manifest.json')
+    .pipe(dest(folder.build))
 }
 
 function html() {
-  return src(folder.src.root + '/index.html')
+  return src(folder.src + '/index.html')
     .pipe(replace(/\<\!\-\-\ css\-block\ \-\-\>([\s\S]*)\<\!\-\-\ end\-css\-block\ \-\-\>/mg, '<link rel="stylesheet" href="css/' + filename.css + '">'))
     .pipe(replace(/\<\!\-\-\ js\-block\ \-\-\>([\s\S]*)\<\!\-\-\ end\-js\-block\ \-\-\>/mg, '<script src="js/' + filename.js + '"></script>'))
-    .pipe(dest(folder.build.root))
+    .pipe(dest(folder.build))
 }
 
 function fonts() {
-  return src(folder.src.fonts + '/**/*.*')
-    .pipe(dest(folder.build.fonts))
+  return src(folder.src + '/fonts/**/*.*')
+    .pipe(dest(folder.build + '/fonts'))
 }
 
 function icons() {
-  return src(folder.src.icons + '/**/*.*')
-    .pipe(dest(folder.build.icons))
+  return src(folder.src + '/icons/**/*.*')
+    .pipe(dest(folder.build + '/icons'))
 }
 
 function css() {
-  return src(folder.src.css + '/*.css')
+  return src(folder.src + '/css/*.css')
     .pipe(concat(filename.css))
-    .pipe(minifyCSS({
-      sourceMap: true
-    }))
-    .pipe(dest(folder.build.css))
+    .pipe(minifyCSS())
+    .pipe(dest(folder.build + '/css'))
 }
 
 function js() {
   return src([
-      folder.src.js + '/vendor/*.js',
-      folder.src.js + '/helper.js',
-      folder.src.js + '/data.js',
-      folder.src.js + '/fontawesome.js',
-      folder.src.js + '/update.js',
-      folder.src.js + '/state.js',
-      folder.src.js + '/bookmarks.js',
-      folder.src.js + '/control.js',
-      folder.src.js + '/menu.js',
-      folder.src.js + '/header.js',
-      folder.src.js + '/modal.js',
-      folder.src.js + '/tip.js',
-      folder.src.js + '/shade.js',
-      folder.src.js + '/theme.js',
-      folder.src.js + '/date.js',
-      folder.src.js + '/greeting.js',
-      folder.src.js + '/transitional.js',
-      folder.src.js + '/clock.js',
-      folder.src.js + '/search.js',
-      folder.src.js + '/link.js',
-      folder.src.js + '/version.js',
-      folder.src.js + '/keyboard.js',
-      folder.src.js + '/background.js',
-      folder.src.js + '/layout.js',
-      folder.src.js + '/auto-suggest.js',
-      folder.src.js + '/pagelock.js',
-      folder.src.js + '/edge.js',
-      folder.src.js + '/init.js',
+      folder.src + '/js/vendor/*.js',
+      folder.src + '/js/helper.js',
+      folder.src + '/js/data.js',
+      folder.src + '/js/fontawesome.js',
+      folder.src + '/js/update.js',
+      folder.src + '/js/state.js',
+      folder.src + '/js/bookmarks.js',
+      folder.src + '/js/control.js',
+      folder.src + '/js/menu.js',
+      folder.src + '/js/header.js',
+      folder.src + '/js/modal.js',
+      folder.src + '/js/tip.js',
+      folder.src + '/js/shade.js',
+      folder.src + '/js/theme.js',
+      folder.src + '/js/date.js',
+      folder.src + '/js/greeting.js',
+      folder.src + '/js/transitional.js',
+      folder.src + '/js/clock.js',
+      folder.src + '/js/search.js',
+      folder.src + '/js/link.js',
+      folder.src + '/js/version.js',
+      folder.src + '/js/keyboard.js',
+      folder.src + '/js/background.js',
+      folder.src + '/js/layout.js',
+      folder.src + '/js/auto-suggest.js',
+      folder.src + '/js/pagelock.js',
+      folder.src + '/js/edge.js',
+      folder.src + '/js/init.js',
     ])
     .pipe(concat(filename.js))
     .pipe(uglify())
-    .pipe(dest(folder.build.js, {
+    .pipe(dest(folder.build + '/js', {
       sourcemaps: '.'
     }))
 }
