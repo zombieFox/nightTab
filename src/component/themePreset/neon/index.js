a62ba64e5e1d2c19814a102df850e4704fb5ebf2
zombieFox <crudetoaster@gmail.com>
2021-08-18 19:38:53 +0100

    improve theme preset

--- src/component/themePreset/neon/index.js
+++ src/component/themePreset/neon/index.js
@@ -1,7 +1,7 @@
 export const neon = {
   name: 'Neon',
-  color: { range: { primary: { h: 219, s: 45, l: 22 } }, contrast: { start: 15, end: 93 } },
-  accent: { hsl: { h: 330, s: 100, l: 50 }, rgb: { r: 255, g: 0, b: 128 } },
+  color: { range: { primary: { h: 219, s: 45, l: 22 } }, contrast: { start: 15, end: 85 } },
+  accent: { hsl: { h: 192, s: 100, l: 50 }, rgb: { r: 0, g: 204, b: 255 } },
   font: { display: { name: 'Dosis', weight: 300, style: 'normal' }, ui: { name: 'Inria Sans', weight: 300, style: 'normal' } },
   background: {
     type: 'image',
@@ -11,8 +11,7 @@ export const neon = {
       start: { hsl: { h: 206, s: 16, l: 40 }, rgb: { r: 86, g: 104, b: 118 } },
       end: { hsl: { h: 219, s: 28, l: 12 }, rgb: { r: 22, g: 28, b: 39 } }
     },
-    // image: { url: 'https://github.com/zombieFox/nightTabAssets/blob/main/images/1629106513649.jpeg?raw=true', blur: 0, opacity: 20, scale: 100, grayscale: 0, accent: 0, vignette: { opacity: 0, start: 90, end: 70 } },
-    image: { url: 'https://i.redd.it/alebbb3ho4c51.png', blur: 0, opacity: 20, scale: 100, grayscale: 0, accent: 0, vignette: { opacity: 0, start: 90, end: 70 } },
+    image: { url: 'https://github.com/zombieFox/nightTabAssets/blob/main/images/1629141035201.jpeg?raw=true', blur: 0, opacity: 20, scale: 100, grayscale: 0, accent: 0, vignette: { opacity: 0, start: 90, end: 70 } },
     video: { url: '', blur: 4, opacity: 20, scale: 100, grayscale: 0, accent: 0, vignette: { opacity: 0, start: 90, end: 70 } }
   },
   radius: 25,
--- src/component/themePreset/obsidian/index.js
+++ src/component/themePreset/obsidian/index.js
@@ -4,14 +4,14 @@ export const obsidian = {
   accent: { hsl: { h: 180, s: 100, l: 50 }, rgb: { r: 0, g: 255, b: 255 } },
   font: { display: { name: 'Zilla Slab', weight: 700, style: 'normal' }, ui: { name: 'Montserrat', weight: 400, style: 'normal' } },
   background: {
-    type: 'theme',
+    type: 'image',
     color: { hsl: { h: 221, s: 47, l: 17 }, rgb: { r: 23, g: 36, b: 64 } },
     gradient: {
       angle: 160,
       start: { hsl: { h: 206, s: 16, l: 40 }, rgb: { r: 86, g: 104, b: 118 } },
       end: { hsl: { h: 219, s: 28, l: 12 }, rgb: { r: 22, g: 28, b: 39 } }
     },
-    image: { url: '', blur: 0, grayscale: 0, scale: 100, accent: 0, opacity: 100, vignette: { opacity: 0, start: 90, end: 70 } },
+    image: { url: 'https://github.com/zombieFox/nightTabAssets/blob/main/images/1629141031788.jpeg?raw=true', blur: 0, opacity: 10, scale: 100, grayscale: 0, accent: 0, vignette: { opacity: 0, start: 90, end: 70 } },
     video: { url: '', blur: 0, grayscale: 0, scale: 100, accent: 0, opacity: 100, vignette: { opacity: 0, start: 90, end: 70 } }
   },
   radius: 25,
