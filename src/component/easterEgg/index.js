3db6a9943b4b64ddc6627b1362b3ce21e76715c0
zombieFox <crudetoaster@gmail.com>
2021-08-21 10:47:33 +0100

    more random easter egg

--- src/component/easterEgg/index.js
+++ src/component/easterEgg/index.js
@@ -40,6 +40,10 @@ easterEgg.toaster.render = () => {
 
   newBookmarkData.link.background.image.url = 'https://github.com/zombieFox/nightTabAssets/blob/main/images/1628494879270.gif?raw=true';
 
+  newBookmarkData.link.display.name.show = false;
+
+  newBookmarkData.link.display.visual.show = false;
+
   newBookmarkData.link.accent.by = 'custom';
 
   newBookmarkData.link.accent.hsl = { h: randomNumber(0, 360), s: 100, l: 50 };
@@ -51,6 +55,10 @@ easterEgg.toaster.render = () => {
   newBookmarkData.link.color.hsl = { h: 0, s: 0, l: 100 };
 
   newBookmarkData.link.color.rgb = { r: 255, g: 255, b: 255 };
+
+  newBookmarkData.link.shape.wide = Math.random() < 0.5;
+
+  newBookmarkData.link.shape.tall = Math.random() < 0.5;
 
   bookmark.item.mod.add(newBookmarkData);
 
