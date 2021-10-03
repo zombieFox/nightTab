4f3a741fc11a0c53d4a2ecf89954874f6e9ac798
metruzanca <samuele@zanca.dev>
2021-10-02 20:05:02 -0400

    Fixed bug breaking nighttab on lint
    reset.css wasn't being loaded as it was imported incorrectly

--- src/component/index.js
+++ src/component/index.js
@@ -1,51 +1,25 @@
-/* eslint-disable */
-import { reset } from './reset';
+import './index.css'
 
-import { Button } from './button';
-import { Collapse } from './collapse';
-import { Dropdown } from './dropdown';
-import { Edge } from './edge';
-import { KeyboardShortcut } from './keyboardShortcut';
-import { Modal } from './modal';
-import { Shade } from './shade';
-import { Suggest } from './suggest';
-import { Tab } from './tab';
-import { ToolbarControl } from './toolbarControl';
-import { Link } from './link';
+import { base } from './base'
+import { state } from './state'
+import { data } from './data'
+import { version } from './version'
+import { fontawesome } from './fontawesome'
+import { icon } from './icon'
+import { keyboard } from './keyboard'
+import { layout } from './layout'
+import { logo } from './logo'
+import { menu } from './menu'
+import { pageLock } from './pageLock'
+import { theme } from './theme'
+import { update } from './update'
+import { bookmark } from './bookmark'
+import { header } from './header'
+import { group } from './group'
+import { toolbar } from './toolbar'
+import { groupAndBookmark } from './groupAndBookmark'
 
-import { Control_checkbox } from './control/checkbox';
-import { Control_color } from './control/color';
-import { Control_colorMixer } from './control/colorMixer';
-import { Control_groupText } from './control/groupText';
-import { Control_helperText } from './control/helperText';
-import { Control_inputButton } from './control/inputButton';
-import { Control_radio } from './control/radio';
-import { Control_radioGrid } from './control/radioGrid';
-import { Control_slider } from './control/slider';
-import { Control_sliderSlim } from './control/sliderSlim';
-import { Control_text } from './control/text';
-import { Control_select } from './control/select';
-
-import { base } from './base';
-import { state } from './state';
-import { data } from './data';
-import { version } from './version';
-import { fontawesome } from './fontawesome';
-import { icon } from './icon';
-import { keyboard } from './keyboard';
-import { layout } from './layout';
-import { logo } from './logo';
-import { menu } from './menu';
-import { pageLock } from './pageLock';
-import { theme } from './theme';
-import { update } from './update';
-import { bookmark } from './bookmark';
-import { header } from './header';
-import { group } from './group';
-import { toolbar } from './toolbar';
-import { groupAndBookmark } from './groupAndBookmark';
-
-import * as form from './form';
+import * as form from './form'
 
 export const component = {
   base,
@@ -67,4 +41,4 @@ export const component = {
   form,
   toolbar,
   groupAndBookmark
-};
+}
