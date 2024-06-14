7dd40ea0a7aef52bfa506d5bcd45de5227a8f49c
zombieFox <kuldeep.matharu@gmail.com>
2024-06-14 09:37:32 +0000

    Deploying to gh-pages from @ zombieFox/nightTab@ff6df201979170afeb02a8c5557598d247d41849 🚀

---
+++ initialBackground.js
@@ -0,0 +1,1 @@
+if(localStorage.getItem("nightTabStyle")){const e=document.createElement("style");switch(e.type="text/css",e.media="screen",localStorage.getItem("nightTabStyle")){case"light":e.innerHTML="html, body {background-color: rgb(255, 255, 255);}";break;case"dark":e.innerHTML="html, body {background-color: rgb(0, 0, 0);}"}document.querySelector("head").appendChild(e)}
